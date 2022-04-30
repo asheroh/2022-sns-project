8450842701e13eb61f5190274c995a2af4dc81bf
AsherOh <wnstj701@naver.com>
2022-04-30 18:17:49 +0900

    멋사 3주차 과제1

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def showmain(request):
         return render(request, 'main/show.html')
+
+def showwrite(request):
+return render(request, 'main/writepage.html')
+
+def showhistory(request):       
+return render(request, 'main/historypage.html')
