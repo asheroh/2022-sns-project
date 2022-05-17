5fd8b9a794d9c0c05e1bfb3397eff0fc88933439
AsherOh <wnstj701@naver.com>
2022-05-17 17:34:43 +0900

    5주차 과제 추가 수정

--- sns_project/settings.py
+++ sns_project/settings.py
@@ -128,3 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# 이용자가 업로드한 파일을 모으는 곳
+MEDIA_URL = '/media/'
--- sns_project/urls.py
+++ sns_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('main.urls')),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('<int:id>', views.detail, name="detail"),
     path('new/', views.new, name="new"),
     path('create/', views.create, name="create"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
