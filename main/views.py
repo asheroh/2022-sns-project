0972eaa07c7ad337662291deb9ae2ae45a93d1ad
AsherOh <wnstj701@naver.com>
2022-07-20 17:40:39 +0900

    sns-project 5차 수정

--- main/views.py
+++ main/views.py
@@ -18,20 +18,12 @@ def showwrite(request):
     return render(request, 'main/writepage.html')
 
 
-def showhistory(request):
-    return render(request, 'main/historypage.html')
-
-
 def showphoto(request):
     return render(request, 'main/photopage.html')
 
 
-def showcharacter(request):
-    return render(request, 'main/characterpage.html')
-
-
-def showhobby(request):
-    return render(request, 'main/hobbypage.html')
+def showmbti(request):
+    return render(request, 'main/mbti.html')
 
 
 def detail(request, id):
